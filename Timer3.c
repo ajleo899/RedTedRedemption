059db23ca3a32777c22f8dc8d2add4d902a1c8e5
Adit Jain <ajleo899@gmail.com>
2019-04-11 23:21:11 -0500

    Add files via upload

---
+++ Timer3.c
@@ -0,0 +1,48 @@
+// TIMER3.c
+// Runs on LM4F120/TM4C123
+// Use TIMER3 in 32-bit periodic mode to request interrupts at a periodic rate
+// Daniel Valvano
+// Last Modified: 3/6/2015 
+// You can use this timer only if you learn how it works
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+  Program 7.5, example 7.6
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+
+// ***************** TIMER3_Init ****************
+// Activate TIMER3 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq)
+// Outputs: none
+void TIMER3_Init(uint32_t period){
+  SYSCTL_RCGCTIMER_R |= 0x08;   // 0) activate TIMER3
+  TIMER3_CTL_R = 0x00000000;    // 1) disable TIMER3A during setup
+  TIMER3_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER3_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER3_TAILR_R = period-1;    // 4) reload value
+  TIMER3_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER3_ICR_R = 0x00000001;    // 6) clear TIMER3A timeout flag
+  TIMER3_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00040000; // 8) priority 7
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+  TIMER3_CTL_R = 0x00000001;    // 10) enable TIMER3A
+}
+
---
+++ button.d
@@ -0,0 +1,3 @@
+.\button.o: Button.c
+.\button.o: S:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\button.o: ../inc/tm4c123gh6pm.h
